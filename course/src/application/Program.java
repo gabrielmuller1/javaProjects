ac504e543497618da499d17c614b71b6b41e6528
Gabriel Cezar Muller <81782103+gabrielmuller1@users.noreply.github.com>
2021-10-18 10:13:48 -0300

    Add files via upload

--- course/src/application/Program.java
+++ course/src/application/Program.java
@@ -12,20 +12,35 @@ public class Program {
 		Scanner sc = new Scanner(System.in);
 
 		Product product = new Product();
-
-		System.out.println(" Entre os dados do produto:  ");
-		System.out.print(" Nome:  ");
+		
+		System.out.println("Digite os dados do produto: ");
+		System.out.print("Nome do produto: ");
 		product.nome = sc.nextLine();
-
-		System.out.print(" Preço:  ");
+		
+		System.out.print("Preço: ");
 		product.preco = sc.nextDouble();
-
-		System.out.print(" Quantidade no estoque:  ");
+		
+		System.out.print("Quantidade no estoque: ");
 		product.qtd = sc.nextInt();
-
-		System.out.println(product);
-
+		
+		System.out.println();
+		System.out.println("Dados do produto: " + product);
+		
+		System.out.println();
+		System.out.print("Digite o número de produtos que serão adicionados no estoque: ");
+		int qtd = sc.nextInt();
+		product.addProducts(qtd);
+		
+		System.out.println();
+		System.out.println("Dados atulizados: " + product);
+		
+		System.out.println();
+		System.out.print("Digite o número de produtos que serão removidos: ");
+		qtd = sc.nextInt();
+		product.removeProducts(qtd);
+		
+		System.out.println();
+		System.out.println("Dados atualizados: " + product);
 		sc.close();
 	}
-
 }
